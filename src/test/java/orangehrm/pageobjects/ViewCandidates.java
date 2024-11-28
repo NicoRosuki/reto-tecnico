59b0f1fa3a004765d301a5132b3ce04b51b48e85
Nicolas Rossi <nicolas.rossi@globallogic.com>
2024-11-28 18:54:13 -0300

    Ultimos_cambios

--- src/test/java/orangehrm/pageobjects/ViewCandidates.java
+++ src/test/java/orangehrm/pageobjects/ViewCandidates.java
@@ -8,4 +8,22 @@ public class ViewCandidates extends PageObject {
   public static final Target ADD_BTN = Target.the("Add Button")
       .locatedBy("//button[text()=' Add ']");
 
+  public static final Target KEYWORDS = Target.the("Keywords")
+      .locatedBy("[placeholder='Enter comma seperated words...']");
+
+  public static final Target SEARCH_BTN = Target.the("Add Button")
+      .locatedBy("//button[text()=' Search ']");
+
+  public static final Target VACANCY_CELL = Target.the("Vacancy Name Cell")
+      .locatedBy("//div[@class='oxd-table-card']//div[2]/div");
+
+  public static final Target CANDIDATE_CELL = Target.the("Candidate Name Cell")
+      .locatedBy("//div[@class='oxd-table-card']//div[3]/div");
+
+  public static final Target DATE_CELL = Target.the("Date Cell")
+      .locatedBy("//div[@class='oxd-table-card']//div[5]/div");
+
+  public static final Target STATUS_CELL = Target.the("Candidate Status Cell")
+      .locatedBy("//div[@class='oxd-table-card']//div[6]/div");
+
 }
--- src/test/resources/features/manage_candidates.feature
+++ src/test/resources/features/manage_candidates.feature
@@ -3,11 +3,6 @@ Feature: Manage Candidates
   Scenario: Hire a candidate
     Given HiringManager is on the login page
     When he logs in with valid credentials
-    And he navigates to the Recruitment page
-    And he adds a new candidate with valid details
-    And he shortlists the candidate
-    And he schedules an interview for the candidate
-    And he marks the interview as passed
-    And he offers the candidate a job
-    And he hires the candidate
-    Then he sees the candidate has the status "Hired"
+    And he adds the new candidate "Juan" "Perez" with email "jperez1234@email.com"
+    And he completes the hiring process for the candidate
+    Then he sees the candidate has the status "Hired" and candidate data is correct
